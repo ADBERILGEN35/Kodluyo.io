0f1095a3baf5794c7d4196c09247b71d0452029d
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2021-05-26 15:50:30 +0300

    Hrms
    Hrsm uygulaması geliştirilmeye devam edildi.

--- Hrms/src/main/java/kodlamaio/Hrms/dataAccess/abstracts/TitleDao.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.Hrms.dataAccess.abstracts;
-
-import kodlamaio.Hrms.entities.concretes.Title;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TitleDao extends JpaRepository<Title,Integer> {
-}
